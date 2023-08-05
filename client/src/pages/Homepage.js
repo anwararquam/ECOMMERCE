8376f2b7f2d5d966786dd0251a733e744ed5eefd
anwararquam <140526436+anwararquam@users.noreply.github.com>
2023-08-05 13:30:51 +0530

    context api setup and bit use in homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import Layout from '../components/Layouts/layout'
-const homepage = () => {
+import Layout from '../components/Layouts/layout';
+import { useAuth } from '../context/auth';
+const Homepage = () => {
+  const [auth,setAuth]=useAuth()
   return (
     <Layout title={'Best Offers'}>
       <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth,null,4)}</pre>
     </Layout>
   )
 }
 
-export default homepage
+export default Homepage
