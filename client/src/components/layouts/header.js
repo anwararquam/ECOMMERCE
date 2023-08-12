611df3d20f2016bd3c6561f4ecd6acbc3dbc75df
anwararquam <140526436+anwararquam@users.noreply.github.com>
2023-08-12 16:07:40 +0530

    dropdown menu setup

--- client/src/components/layouts/header.js
+++ client/src/components/layouts/header.js
@@ -29,6 +29,8 @@ const Header = () => {
         </li>
         {
           !auth.user ? (<>
+            
+
             <li className="nav-item">
           <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
         </li>
@@ -36,9 +38,16 @@ const Header = () => {
           <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
         </li>
           </>):(<>
-            <li className="nav-item">
-          <NavLink onClick={handleLogout}to="/login" className="nav-link" href="#">Logout</NavLink>
-        </li>
+            <li className="nav-item dropdown">
+  <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    {auth?.user?.name}
+  </NavLink>
+  <ul className="dropdown-menu">
+    <li><NavLink to="/dashboard"className="dropdown-item" >Dashboard</NavLink></li>
+    <li><NavLink onClick={handleLogout}to="/login" className="dropdown-item" href="#">Logout</NavLink></li>
+  </ul>
+</li>
+           
           </>)
         }
         <li className="nav-item">
