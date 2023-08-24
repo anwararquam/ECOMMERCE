e602c4d423533ce2f6f9d136c2d13e21d5349704
anwararquam <140526436+anwararquam@users.noreply.github.com>
2023-08-24 22:37:46 +0530

    backend product api

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,15 +36,20 @@ try {
 }
 }
 //update product present in the product model
-export const updateproduct=async(req,res)=>{
+export const productphotocontroller=async(req,res)=>{
     try {
+        const product=await Productmodel.findById(req.params.pid).select("photo");
+        if(product.photo.data){
+            res.set('Content-type',product.photo.contentType)
+            return res.status(200).send(product.photo.data);
+        }
         
     } catch (error) {
         console.log(error),
         res.status(500).send({
             success:false,
+            message:"Error while getting photos",
             error,
-            message:"Unable to Update product",
             
         })
     }
@@ -91,15 +96,51 @@ export const getproductcontroller=async(req,res)=>{
     }
 }
 //delete product
-export const deleteproduct=async(req,res)=>{
+export const deleteproductcontroller=async(req,res)=>{
         try {
-            
+            await Productmodel.findByIdAndDelete(req.params.pid).select("-photo");
+            res.status(200).send({
+                success:true,
+                message:"Product deleted successfully"
+            });
         } catch (error) {
             console.log(error),
             res.status(500).send({
                 success:false,
+                message:"Unable to delete the product",
                 error,
-                message:"Unable to dlete the product",
             })
         }
+}
+export const updateproductcontroller=async(req,res)=>{
+    try {
+        const {name,description,price,category,quantity} =req.fields
+        const {photo}=req.files
+        switch(true){
+            case !name:res.status(500).send({error:'Name is Required'})
+            case !description:res.status(500).send({error:'Description is Required'})
+            case !price:res.status(500).send({error:'Price is Required'})
+            case !category:res.status(500).send({error:'Category is Required'})
+            case !quantity:res.status(500).send({error:'Quantity is Required'})
+            case !photo && photo.size>1000:res.status(500).send({error:'Photo is Required && Size should be less than 1mb'})
+        }
+        const product= await Productmodel.findByIdAndUpdate(req.params.pid,{...req.fields,slug:slugify(name)},{new:true})
+        if(photo){
+            product.photo.data=fs.readFileSync(photo.path);
+            product.photo.contentType=photo.type;
+        }
+         await product.save();
+         res.status(201).send({
+            success:true,
+            message:"Product Updated Successfully",
+            product
+         })
+    } catch (error) {
+        console.log(error),
+        res.status(500).send({
+            success:false,
+            message:"Unable to Update the Product",
+            error
+        })
+    }
 }
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { isAdmin, requiresignIn } from '../middlewares/authmiddleware.js';
 import formidable from "express-formidable"
-import { createproductcontroller, getproductcontroller, getsingleproductcontroller } from '../controllers/productController.js';
+import { createproductcontroller, deleteproductcontroller, getproductcontroller, getsingleproductcontroller, productphotocontroller, updateproductcontroller } from '../controllers/productController.js';
 const router=express.Router()
-router.post('/create-product',requiresignIn,isAdmin,formidable(),createproductcontroller)
-router.post('/update-product',isAdmin,requiresignIn,formidable(),createproductcontroller);
+router.post('/create-product',requiresignIn,isAdmin,formidable(),createproductcontroller);
+router.put('/update-product/:pid',requiresignIn,isAdmin,formidable(),updateproductcontroller)
+router.get('/product-photo/:pid',productphotocontroller);
 router.get('/get-product',getproductcontroller);
 router.get('/get-product/:slug',getsingleproductcontroller);
+router.delete('/delete-product/:pid',requiresignIn,isAdmin,deleteproductcontroller);
 export default router
